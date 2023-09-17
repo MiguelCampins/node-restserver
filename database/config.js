f9f566d8e714ac5919c17b8627ddfdb6a3115549
Miguel Campins <miguel_ilutec@hotmail.com>
2023-09-17 12:43:22 +0200

    error con la base de datos

--- database/config.js
+++ database/config.js
@@ -1,19 +1,21 @@
 const mongoose = require('mongoose');
 
-const dbConnection = async () => {
+
+
+const dbConnection = async() => {
 
     try {
 
-        await mongoose.connect( process.env.MONGODB_CNN, {
-            
-        } );
+        await mongoose.connect( process.env.MONGODB_CNN);
+    
+        console.log('Base de datos online');
 
-        console.log('Base de datos conectada')
-        
     } catch (error) {
-        console.log(error)
-        throw new Error('Error al iniciar la base de datos')
+        console.log(error);
+        throw new Error('Error a la hora de iniciar la base de datos');
     }
+
+
 }
 
 
